23aea34fa6244ec41629336852274834fb813762
EileenXXX <jxu22@inside.artcenter.edu>
2023-10-19 16:57:08 -0700

    week 6 inclass
    week 6 inclass

---
+++ week6/inclass/sketch.js
@@ -0,0 +1,40 @@
+let particles = []
+
+function setup(){
+    createCanvas(700, 600)
+    colorMode(HSB, TWO_PI, 1, 1)
+    //velocity = createVectore(random(-4, 4), random(-4, 4))
+
+    particles[0] = new Particle()
+    wind = createVector(-0.005, 0)
+    gravity = createVector(0, 0.01)
+}
+
+function draw(){
+    background(1, 0, 0)
+
+    particles.forEach((part)=>{
+        part.display()
+        part.addForce(wind)
+        part.addForce(gravity)
+        part.attractToMouse()
+        part.move()
+        part.reachOut(particles)
+    })
+}
+
+function mouseReleased() {
+    //John.position.set(mouseX, mouseY)
+    particles.push(new Particle(mouseX, mouseY))
+}
+
+function keyReleased(){
+    if(keyCode == RIGHT_ARROW){
+        wind.x += 0.01
+
+    }
+    else if(keyCode == LEFT_ARROW){
+        wind.x -=0.01
+    }
+
+}
